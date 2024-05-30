ba7fcb697e1a0399caec8a632fae64a37edc0bcf
nehru00123@gmail.com <nehru00123@gmail.com>
2024-05-30 08:48:32 +0530

    carosel model added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import Navigation from './customer/components/Navigation/Navigation';
+import HomePage from './customer/pages/HomePage/HomePage';
 
 function App() {
   return (
     <div className="App">
-    <Navigation></Navigation>
+    <Navigation/>
+    <div>
+      <HomePage/>
+    </div>
 
     </div>
   );
---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainCarousel from '../../components/HomeCarosel/MainCarossel'
+
+const HomePage = () => {
+  return (
+    <div>
+      <MainCarousel></MainCarousel>
+      <div>
+        other section
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
